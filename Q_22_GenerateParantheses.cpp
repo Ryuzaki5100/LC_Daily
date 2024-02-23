59c80ebc4e402db2bc0fff0ed22d198bdd1cb6e2
atharva gulkotwar <atharvaa2210@gmail.com>
2024-02-23 18:55:58 +0530

    Question 22 : Generate Parantheses Solution Added

---
+++ Q_22_GenerateParantheses.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+void helper(vector<string> &result, string soFar, int open, int close)
+{
+    if (open == 0 && close == 0)
+    {
+        result.push_back(soFar);
+        return;
+    }
+    if (open > 0)
+        helper(result, soFar + "(", open - 1, close);
+    if (open < close)
+        helper(result, soFar + ")", open, close - 1);
+}
+vector<string> generateParantheses(int n)
+{
+    vector<string> result;
+    helper(result, "", n, n);
+    return result;
+}
+int main()
+{
+}
